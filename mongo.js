040494744858710d77d415b943ea4dbaa0247f80
Ra942 <53893437+Ra942@users.noreply.github.com>
2019-10-30 22:39:35 -0700

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mongodb_test";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("test_no1");
+  var myobj = { name: "web project ", address: "s3 mca",phone:"1234567890" };
+  dbo.collection("test_no1").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted succesfully");
+    db.close();
+  });
+});
+
